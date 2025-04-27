ac6a567541a8c46cc1e2d3bbed773c3eafd3c37b
tanathon-101 <tanaton.akko@gmail.com>
2025-04-27 15:21:25 +0700

    add validation

--- Models/Movies.cs
+++ Models/Movies.cs
@@ -13,6 +13,8 @@ namespace MovieMvc.Models
         public DateTime ReleaseDate { get; set; }
         [Required]
         public string Genre { get; set; }
+        [Range(1, 1000, ErrorMessage = "Duration must be between 1 and 1000 minutes")]
+
         public int Duration { get; set; }
         public DateTime? CreateDate { get; set; }
         public DateTime? ModifyDate { get; set; }
